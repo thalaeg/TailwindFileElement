10f6ec5aae83270522cae2476c7bd6d781db1ef6
Erich Thalacker <egthalacker@gmail.com>
2022-03-21 18:10:55 -0500

    Add project files.

---
+++ TailwindFileTest/tailwind.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+    content: [
+        "./**/*.{html,razor,cshtml,cs}",
+    ],    
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+    ],
+}
